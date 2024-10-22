f44a3d083523fb60d0016540ce7b8a855b319dcc
PhillipXT <phillip.theriault@spellweaver.com>
2024-10-22 17:19:42 -0600

    Created initial game window

--- main.py
+++ main.py
@@ -3,13 +3,33 @@ import pygame
 from constants import *
 
 def main():
+
 	print("Starting asteroids!")
 	print(f"Screen width: {SCREEN_WIDTH}")
 	print(f"Screen height: {SCREEN_HEIGHT}")
 
-#
-# Ensure we only run when executed directly, not when imported as a module:
-#
+	(numpass, numfail) = pygame.init()
 
+	print (f"Pygame initialized:")
+	print (f"Modules passed: {numpass}")
+	print (f"Modules failed: {numfail}")
+
+	# Get a new GUI window
+	screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+
+	while True:
+
+		# Check for system messages
+		for event in pygame.event.get():
+			if event.type == pygame.QUIT:
+				return
+
+		# Fill the screen with the background colour	
+		screen_rect = screen.fill(BACKGROUND_COLOUR, None, 0)
+
+		# Refresh the screen
+		pygame.display.flip()
+
+# Ensure we only run when executed directly, not when imported as a module:
 if __name__ == "__main__":
 	main()
