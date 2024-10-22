519f9a3fafba47caeff9deb6df1a9b27c211cb07
Phillip Theriault <phillip.theriault@spellweaver.com>
2024-10-22 10:38:47 -0600

    Added constants and imported into main

--- main.py
+++ main.py
@@ -1,7 +1,11 @@
 import pygame
 
+from constants import *
+
 def main():
 	print("Starting asteroids!")
+	print(f"Screen width: {SCREEN_WIDTH}")
+	print(f"Screen height: {SCREEN_HEIGHT}")
 
 #
 # Ensure we only run when executed directly, not when imported as a module:
