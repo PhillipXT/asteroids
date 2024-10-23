67445479884fb13432ee686302a7ced0e4d29137
PhillipXT <phillip.theriault@spellweaver.com>
2024-10-22 19:19:05 -0600

    Added base class for game objects

---
+++ circleshape.py
@@ -0,0 +1,23 @@
+import pygame
+
+# Base class for game objects
+class CircleShape(pygame.sprite.Sprite):
+
+	def __init__(self, x, y, radius):
+		
+		if hasattr(self, "containers"):
+			super().__init__(self.containers)
+		else:
+			super().__init__()
+
+		self.position = pygame.Vector2(x, y)
+		self.velocity = pygame.Vector2(0, 0)
+		self.radius = radius
+
+	# Subclasses must override
+	def draw(self, screen):
+		pass
+
+	# Subclasses must override
+	def update(self, delta):
+		pass
